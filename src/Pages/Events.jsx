55d27aa593913a1cb7f3dc118eebd5e19d178f19
Raval Vandan <vandanraval2002@gmail.com>
2024-07-28 00:02:40 +0530

    Event Page Updated

--- src/Pages/Events.jsx
+++ src/Pages/Events.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import { SlLocationPin } from "react-icons/sl";
 import crosImg from "../assests/image/shape8.svg";
 const Events = () => {
-  const [activeTab, setActiveTab] = useState('ALL');
+  const [activeTab, setActiveTab] = useState("ALL");
   return (
     <div className="event-page-container">
       <div className="custom-page-title-section">
@@ -16,67 +16,98 @@ const Events = () => {
       </div>
 
       <div className="tab-container">
-      <div 
-          className={`tab ${activeTab === 'ALL' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('ALL')}
-      >
+        <div
+          className={`tab ${activeTab === "ALL" ? "active" : ""}`}
+          onClick={() => setActiveTab("ALL")}
+        >
           ALL
-      </div>
-      <div 
-          className={`tab ${activeTab === 'Happening' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('Happening')}
-      >
+        </div>
+        <div
+          className={`tab ${activeTab === "Happening" ? "active" : ""}`}
+          onClick={() => setActiveTab("Happening")}
+        >
           Happening
-      </div>
-      <div 
-          className={`tab ${activeTab === 'Upcoming' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('Upcoming')}
-      >
+        </div>
+        <div
+          className={`tab ${activeTab === "Upcoming" ? "active" : ""}`}
+          onClick={() => setActiveTab("Upcoming")}
+        >
           Upcoming
-      </div>
-      <div 
-          className={`tab ${activeTab === 'Expired' ? 'active' : ''}`} 
-          onClick={() => setActiveTab('Expired')}
-      >
+        </div>
+        <div
+          className={`tab ${activeTab === "Expired" ? "active" : ""}`}
+          onClick={() => setActiveTab("Expired")}
+        >
           Expired
+        </div>
       </div>
-  </div>
-  <div className="event-container">
-  <div className="event-card">
-
-  <img src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400" alt="Event" className="event-image" />
-  <div className="event-date">June 24, 2020</div>
-  <div className="event-details">
-  <div className="event-title">International Conference on Special Needs Education</div>
-  <div className="event-location"><SlLocationPin className="loc-icon" /> Istanbul, Turkey</div>
-  </div>
-</div>
-<div className="event-card">
-  <img src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400" alt="Event" className="event-image" />
-  <div className="event-date">June 24, 2020</div>
-  <div className="event-details">
-  <div className="event-title">International Conference on Special Needs Education</div>
-  <div className="event-location"><SlLocationPin className="loc-icon"/> Istanbul, Turkey</div>
-  </div>
-</div>
-<div className="event-card">
-  <img src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400" alt="Event" className="event-image" />
-  <div className="event-date">June 24, 2020</div>
-  <div className="event-details">
-  <div className="event-title">International Conference on Special Needs Education</div>
-  <div className="event-location"> <SlLocationPin className="loc-icon" /> Istanbul, Turkey</div>
-  </div>
-</div>
-<div className="event-card">
-  <img src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400" alt="Event" className="event-image" />
-  <div className="event-date">June 24, 2020</div>
-  <div className="event-details">
-  <div className="event-title">International Conference on Special Needs Education</div>
-  <div className="event-location"><SlLocationPin className="loc-icon"/> Istanbul, Turkey</div>
-  </div>
-</div>
-  </div>
-  
+      <div className="event-container">
+        <div className="event-card">
+          <img
+            src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt="Event"
+            className="event-image"
+          />
+          <div className="event-date">June 24, 2020</div>
+          <div className="event-details">
+            <div className="event-title">
+              International Conference on Special Needs Education
+            </div>
+            <div className="event-location">
+              <SlLocationPin className="loc-icon" /> Istanbul, Turkey
+            </div>
+          </div>
+        </div>
+        <div className="event-card">
+          <img
+            src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt="Event"
+            className="event-image"
+          />
+          <div className="event-date">June 24, 2020</div>
+          <div className="event-details">
+            <div className="event-title">
+              International Conference on Special Needs Education
+            </div>
+            <div className="event-location">
+              <SlLocationPin className="loc-icon" /> Istanbul, Turkey
+            </div>
+          </div>
+        </div>
+        <div className="event-card">
+          <img
+            src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt="Event"
+            className="event-image"
+          />
+          <div className="event-date">June 24, 2020</div>
+          <div className="event-details">
+            <div className="event-title">
+              International Conference on Special Needs Education
+            </div>
+            <div className="event-location">
+              {" "}
+              <SlLocationPin className="loc-icon" /> Istanbul, Turkey
+            </div>
+          </div>
+        </div>
+        <div className="event-card">
+          <img
+            src="https://images.pexels.com/photos/5905706/pexels-photo-5905706.jpeg?auto=compress&cs=tinysrgb&w=400"
+            alt="Event"
+            className="event-image"
+          />
+          <div className="event-date">June 24, 2020</div>
+          <div className="event-details">
+            <div className="event-title">
+              International Conference on Special Needs Education
+            </div>
+            <div className="event-location">
+              <SlLocationPin className="loc-icon" /> Istanbul, Turkey
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
