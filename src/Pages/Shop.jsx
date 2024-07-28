b7f2f630658592856eb11708e090021e4bad5431
Raval Vandan <vandanraval2002@gmail.com>
2024-07-28 17:27:33 +0530

    shop page added

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -13,8 +13,53 @@ const Shop = () => {
           <Link to="/">Home</Link> / <strong>Shop</strong>
         </div>
       </div>
-
-      <h1>Shop</h1>
+      <div className="shop-container">
+        <div className="results-info">
+          <p>Showing 1–6 of 7 results</p>
+          <select className="sorting-dropdown">
+            <option value="default">Default sorting</option>
+          </select>
+        </div>
+        <div className="shop-cards-container">
+          <div className="shop-card">
+            <img
+              src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&w=400"
+              alt="Book Cover Softcover"
+            />
+            <span className="sale">Sale!</span>
+            <h2>Book Cover Softcover</h2>
+            <p className="price">
+              <span className="original-price">$210</span> $200
+            </p>
+            <button className="shop-button">ADD TO CART</button>
+          </div>
+          <div className="shop-card">
+            <img
+              src="https://images.pexels.com/photos/6475044/pexels-photo-6475044.jpeg?auto=compress&cs=tinysrgb&w=400"
+              alt="Motivational Book Cover"
+            />
+            <h2>Motivational Book Cover</h2>
+            <p className="price">
+              <span className="original-price">$99</span> $89
+            </p>
+            <button className="shop-button">READ MORE</button>
+          </div>
+          <div className="shop-card">
+            <img
+              src="https://images.pexels.com/photos/2536965/pexels-photo-2536965.jpeg?auto=compress&cs=tinysrgb&w=400"
+              alt="Note Book Mockup"
+            />
+            <h2>Note Book Mockup</h2>
+            <p className="price">$200</p>
+            <button className="shop-button">ADD TO CART</button>
+          </div>
+        </div>
+        <div className="pagination-container">
+          <button className="pagination-button active">1</button>
+          <button className="pagination-button">2</button>
+          <button className="pagination-button">»</button>
+        </div>
+      </div>
     </div>
   );
 };
